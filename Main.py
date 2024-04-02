21422e2d9cf2b27d81fc0bfe11db30b440b03d12
Wirior <46192971+Wirior@users.noreply.github.com>
2024-04-02 17:48:10 +0200

    Added the

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import inquirer  # Importing the inquirer module for interactive user prompts
 from datetime import date  # Importing the date class from the datetime module
 from getpass import getpass # Importing the hidden password module for user input
 import random # Importing random function for slump actions
+import matplotlib.pyplot as plt # importing pylot module to plot grapths
 
 from email_validator import validate_email, EmailNotValidError  # Importing email validation functions
 
@@ -148,6 +149,7 @@ def Account(account_num):
         os.system('cls')
         print("Bankomat \n")
 
+        print(f"Inloggad på: {account_num}")
         questions = [
             inquirer.List('account',
                           message="Välj interaktion",
@@ -241,12 +243,24 @@ def Account_Balance_History(account_num):
     
     for val in Accounts_data[str(account_num)]:
         account_balance = val["balance"]
-        account_transaction = val["transaction"]
         account_date = val["date"]
-        account_note = val["note"]
+       
+    balance = []
+    for index in reversed(account_balance): balance.append(index) # Reversed list of the dates
+    dates = []
+    for index in reversed(account_date): dates.append(index) # Reversed list of the dates
 
-    while True:
-        break  # Placeholder for transaction history functionality
+    title = {'family':'sans-serif','color':'black','size':18}
+    plt.locator_params(axis='x', nbins=4)
+    plt.xticks(rotation=30, ha="right")
+
+    plt.title(f"{str(account_num)}: Saldo historik",loc= 'left', fontdict= title)
+    plt.xlabel("Datum")
+    plt.ylabel("Belopp [sek]")
+
+    plt.plot(dates, balance)
+    plt.show()
+
     return
 
 def Account_Transaction_History(account_num):
